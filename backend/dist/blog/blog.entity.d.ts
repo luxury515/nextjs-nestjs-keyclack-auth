d06edd0b9396fa607ce58e29fab97083c7317485
채병춘 <luxury515@naver.com>
2024-09-21 16:28:14 +0900

    -삭제

--- backend/dist/blog/blog.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Blog {
-    bltn_no: string;
-    bltn_cls_cd: string;
-    titl: string;
-    contt: string;
-    tag: string;
-    inpt_dtm: Date;
-    thumbnail_img_url: string;
-}
--- frontend/app/components/AuthWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthProvider } from '../contexts/AuthContext';
-
-export default function AuthWrapper({ children }: { readonly children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
